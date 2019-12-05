95b46ff608cc5d589f608c17eef3470348aca01c
AnisaMedica <annisah.163040077@mail.unpas.ac.id>
2019-12-05 18:40:46 +0700

    first commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// use App\Http\Controllers\Controller;
+
+class PagesController extends Controller
+{
+  
+    public function home()
+    {
+        return view('index');
+    }
+
+    public function about()
+    {
+        return view('about', ['nama' => 'Anisa Medica']);
+    }
+
+   
+}
